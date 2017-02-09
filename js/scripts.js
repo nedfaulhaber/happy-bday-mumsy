e550ba5fab6a870d3c06471ec8833fd053ff0c95
Ned Faulhaber <nedfaulhaber@gmail.com>
2017-02-08 20:45:15 -0800

    added javascript and mixin for kamasi mobilenav, nav for maya

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 function openNav() {
     document.getElementById("mobile-nav").style.width = "250px";
+    document.getElementById("wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+    document.getElementById("wrapper").style.marginRight= "0";
+}
+
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "250px";
     document.getElementById("kamasi-wrapper").style.marginRight = "250px";
 }
 
