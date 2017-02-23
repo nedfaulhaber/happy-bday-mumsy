3279cf6648d82fa13c72a7948b72ac25fbcedfa0
Ned Faulhaber <nedfaulhaber@gmail.com>
2017-02-22 19:34:13 -0800

    changed font responsiveness for artist pages, adjusted javascript for kamasi page, fixed wrapper/header bug

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,3 @@ function closeNav() {
     document.getElementById("mobile-nav").style.width = "0";
     document.getElementById("wrapper").style.marginRight= "0";
 }
-
-function openNav() {
-    document.getElementById("mobile-nav").style.width = "250px";
-    document.getElementById("kamasi-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mobile-nav").style.width = "0";
-    document.getElementById("kamasi-wrapper").style.marginRight= "0";
-}
