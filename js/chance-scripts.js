34615eeac7b7c120234c63a1020f5613b3e8f6a1
Ned Faulhaber <nedfaulhaber@gmail.com>
2017-03-10 18:41:44 -0800

    finished chance page

---
+++ js/chance-scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("ch-mobile-nav").style.width = "250px";
+    document.getElementById("ch-wrapper").style.marginRight = "250px";
+}
+
+function closeNav() {
+    document.getElementById("ch-mobile-nav").style.width = "0";
+    document.getElementById("ch-wrapper").style.marginRight= "0";
+}
