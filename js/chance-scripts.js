388a4c7a95968a82cd727789af2a47c4797a1d0b
Ned Faulhaber <nedfaulhaber@gmail.com>
2017-04-18 20:45:05 -0700

    switching out all chance pages, scripts, sass, etc, for kendrick

--- js/chance-scripts.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("ch-mobile-nav").style.width = "250px";
-    document.getElementById("ch-wrapper").style.marginRight = "250px";
-}
-
-function closeNav() {
-    document.getElementById("ch-mobile-nav").style.width = "0";
-    document.getElementById("ch-wrapper").style.marginRight= "0";
-}
